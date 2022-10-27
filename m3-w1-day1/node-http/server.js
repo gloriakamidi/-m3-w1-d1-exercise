ce1fc1a9f04fc86309c0b00fc3d9de1ffd39265a
gloriakamidi <g.kamidi.312@westcliff.edu>
2022-10-26 20:19:49 -0700

    w1d1demo

--- m3-w1-day1/node-http/server.js
+++ m3-w1-day1/node-http/server.js
@@ -1,50 +1,50 @@
-//const { fstat } = require('fs');
 var http = require('http');
+var path = require('path');
+var fs = require('fs');
 
 var hostname = 'localhost';
-var port = 3000;
+var port = 3000; 
 
 var server = http.createServer((req, res) => {
    console.log(`request for ${req.url} by method ${req.method}`);
 
-   if (req.method === 'GET') {
-    var fileUrl = req.url;
-    if (fileUrl === '/') {
-        fileUrl = '/index.html';
-    }
+    if (req.method === 'GET') {
+        var fileUrl = req.url;
+        if(fileUrl === '/') {
+            fileUrl = '/index.html';
+        }
 
-    var filePath = path.resolve('./public' + fileUrl);
-    var fileExt = path.extname(filePath);
+        var filePath = path.resolve('./public' + fileUrl);
+        var fileExt = path.extname(filePath);
 
-    if (fileExt === '.html') {
-        fs.acces(filePath, function(err){
+        if(fileExt === '.html') {
+            fs.access(filePath, function(err){
             if (err) {
                 res.statusCode = 404;
                 res.setHeader('Content-Type', 'text/html');
-                res.end(
-                  `<html><body><h1>Error 404: $(fileUrl) not found</h1></body></html>`);
-
-                  return;
+                res.end(`<html><body><h1>Error 404: ${fileUrl} not found</h1></body></html>`);
+                return;
             }
             res.statusCode = 200;
             res.setHeader('Content-Type', 'text/html');
-            fs.createReadStream(filepath).pipe(res);
+            fs.createReadStream(filePath).pipe(res);
         }); 
     } else {
-         res.statusCode = 400;
-         res.setHeader("Content-Type", "text/html");
-         res.end(
-           "<html><body><h1>Error 404: $(fileUrl) is not an html file</h1></body></html>");
-
+         res.statusCode = 404;
+         res.setHeader('Content-Type', 'text/html');
+         res.end(`<html><body><h1>Error 404: ${fileUrl} is not an HTML file</h1></body></html>`);
     } 
-
     } else {
-        res.statusCode = 400;
+        res.statusCode = 404;
         res.setHeader('Content-Type', 'text/html');
-        res.end('<html><body><h1>Error 404: ${req.method} not supported </h1></body></html>');
+        res.end(`<html><body><h1>Error 404: ${req.method} not supported</h1></body></html>`);
 
     }
    });
+
+   server.listen(port, hostname, () => {
+        console.log(`Server running at http://${hostname}:${port}/`);
+   })
   
     
    
